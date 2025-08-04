6055d746f403c85cc608423621222ccb70f9e928
Mahdi Sadeghi <mehgitranstaltor1400@gmail.com>
2025-08-04 21:13:51 +0330

    Added final retouch to client file

--- client.py
+++ client.py
@@ -1,41 +1,35 @@
-# Client side chat room
+# Client Side Chat Room
 import socket
 import threading
-
 
 # Define constants to be used
 DESTINATION_IP = socket.gethostbyname(socket.gethostname())
 DESTINATION_PORT = 12345
-ENCODER = "utf-8"
+ENCODER = 'utf-8'
 BYTESIZE = 1024
 
-# Create client socket
+# Create a client socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((DESTINATION_IP, DESTINATION_PORT))
 
 
 def send_message():
-    """
-        Send a message to the server to be broadcast
-    """
+    '''Send a message to the server to be broadcast'''
     while True:
-        while True:
-            message = input("Hey how are you doing?")
-            client_socket.send(message.decode(ENCODER))
+        message = input("")
+        client_socket.send(message.encode(ENCODER))
 
 
 def recieve_message():
-    """
-        Recieve an incoming message from the server
-    """
+    '''Recieve an incoming message from the server'''
     while True:
         try:
-            # Recieve an incoming message from the server
+            # Recieve an incoming message from the server.
             message = client_socket.recv(BYTESIZE).decode(ENCODER)
 
             # Check for the name flag, else show the message
             if message == "NAME":
-                name = input("What is your name?: ")
+                name = input("What is your name: ")
                 client_socket.send(name.encode(ENCODER))
             else:
                 print(message)
@@ -46,10 +40,10 @@ def recieve_message():
             break
 
 
-# Create thread to continuously send and recieve messages
+# Create threads to continuously send and recieve messages
 recieve_thread = threading.Thread(target=recieve_message)
 send_thread = threading.Thread(target=send_message)
 
 # Start the client
 recieve_thread.start()
-recieve_thread.start()
+send_thread.start()
