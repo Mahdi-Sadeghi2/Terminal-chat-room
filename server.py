7522f6b96944ec4a84581796cf9c47204efc5646
Mahdi Sadeghi <mehgitranstaltor1400@gmail.com>
2025-08-04 20:39:19 +0330

    Added recieve_message function

--- server.py
+++ server.py
@@ -26,14 +26,39 @@ def broadcast_message(message):
     """
         Send a message to all clients connected to the server
     """
-    pass
+    while True:
+        message = input("Hey how are doing?")
 
 
 def recieve_message(client_socket):
     """
         Recieve an incoming message from a specific client and forward the message to be broadcast
     """
-    pass
+    while True:
+        try:
+            # Get the name of the given client
+            index = client_socket_list.index(client_socket)
+            name = client_name_list[index]
+
+            # Recieve message from the client
+            message = client_socket.recv(BYTSIZE).decode(ENCODER)
+            message = f"{name}: {message}".encode(ENCODER)
+            broadcast_message(message)
+        except:
+            # Find the index of the client socket in our list
+            index = client_socket_list.index(client_socket)
+            name = client_socket_list[index]
+
+            # Remove the client socket and name from lists
+            client_socket_list.remove(client_socket)
+            client_name_list.remove(name)
+
+            # Close client socket
+            client_socket.close()
+
+            # Broadcast that the client has left the chat
+            broadcast_message(f"{name} has left the chat!".encode(ENCODER))
+            break
 
 
 def connect_client():
@@ -58,6 +83,11 @@ def connect_client():
             f"{client_name}, you have connected to server!".encode(ENCODER))
         broadcast_message(
             f"{client_name} has joined the chat!".encode(ENCODER))
+
+        # Now that a new client has connected, start a theard
+        recieve_thread = threading.Thread(
+            target=recieve_message, args=(client_socket,))
+        recieve_thread.start()
 
 
 # Start the server
