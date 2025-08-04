8c7f9c1ff4f7ea8e60fb7697c95467fce7c44592
Mahdi Sadeghi <mehgitranstaltor1400@gmail.com>
2025-08-04 16:24:43 +0330

    Added some funtion to client

--- client.py
+++ client.py
@@ -1,1 +1,28 @@
 # Client side chat room
+import socket
+import threading
+
+
+# Define constants to be used
+DESTINATION_IP = socket.gethostbyname(socket.gethostname())
+DESTINATION_PORT = 12345
+ENCODER = "utf-8"
+BYTESIZE = 1024
+
+# Create client socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((DESTINATION_IP, DESTINATION_PORT))
+
+
+def send_message():
+    """
+        Send a message to the server to be broadcast
+    """
+    pass
+
+
+def recieve_message():
+    """
+        Recieve an incoming message from the server
+    """
+    pass
